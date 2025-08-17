e6f26d0b5c848e8b2c8e1175bf5604177eaa559b
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-08-17 16:15:46 +0900

    Added heading role to h1 element

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <main>
-      <h1>MultiCalc</h1>
+      <h1 role="heading">Multi-Calc</h1>
       <OperandContext.Provider value={{ operand, setOperand }}>
         <FormWrapper />
         <CalculatorWrapper />
